187df48b4f1f6fbd14a3f44c51dfff4f1eaad706
Федяй Виталий <vfedyay@hold.bayadera.ua>
2019-10-23 13:00:36 +0300

    create view

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 class Animal {
-  name = ''
-  #health = 100
-  #happiness = 100
-  #eat = 100
-  #drink = 100
-  #sleep = 100
-  #walk = 100
 
-  static getHealth() {
-    console.log(`Hello ${name}!`)
-    console.log('object :', Animal.#health);
+  constructor(name, health, happiness, meals, drink, sleep, walk) {
+    this.name = name
+    this.health = health
+    this.happiness = happiness
+    this.meals = meals
+    this.drink = drink
+    this.sleep = sleep
+    this.walk = walk
+  }
+
+  changeHealth() {
+
   }
 }
 
-let name = prompt('Enter name of your tamagotchi: ')
-if(typeof name === 'string') {
-  let tamagotchi = new Animal(name)
+
+window.onload = function () { 
+  let check = true
+  while(check) {
+    let name = prompt('Enter name of your tamagotchi: ')
+    if( name !== null && name !== '') {
+      let tamagotchi = new Animal(name)
+      document.getElementById('tamagotchiName').innerHTML = name
+      check = false
+    }
+    else {
+      check = true
+    }
+
+  }
+  
 }
-else {
-  throw Error('Error! Please enter name!')
-}
+
+
